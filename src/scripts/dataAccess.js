69319200f742e491610c8d1f0d2a8cbbddd4e90b
Justin Adams <justin@DESKTOP-GP5T8ES.localdomain>
2021-05-11 12:47:43 -0500

    successfully saving permanent state

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -20,3 +20,20 @@ export const getRequests = () => {
   return [...applicationState.requests]
 }
 
+export const sendRequest = (userServiceRequest) => {
+  const fetchOptions = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userServiceRequest)
+  }
+
+
+  return fetch(`${API}/requests`, fetchOptions)
+      .then(response => response.json())
+      .then(() => {
+
+      })
+}
+
