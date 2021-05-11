c3e66962955472b0ffd62d5683dd87da3e9f54b1
Justin Adams <justin@DESKTOP-GP5T8ES.localdomain>
2021-05-11 12:31:39 -0500

    added request form and styled

--- src/scripts/SinkRepair.js
+++ src/scripts/SinkRepair.js
@@ -1,17 +1,17 @@
-import { Requests } from "./Requests.js"
-
+import { Requests } from "./Requests.js";
+import { ServiceForm } from "./ServiceForm.js";
 
 export const SinkRepair = () => {
-    return `
+  return `
         <h1>Maude and Merle's Sink Repair</h1>
         <section class="serviceForm">
+        ${ServiceForm()}
         </section>
 
         <section class="serviceRequests">
-            <h2>Service Requests</h2>
+        <h2>Service Requests</h2>
+        ${Requests()}
+        </section>
+        `;
 
-             ${Requests()}
-
-            </section>
-            `
-        }
+};
